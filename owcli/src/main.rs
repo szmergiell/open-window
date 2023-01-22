c2221ace7d35e5f30eb98be4902b8ee6a6d045b8
szmergiell <szmergiel@pm.me>
2023-01-22 16:11:42 +0100

    primitive command line argument parsing

--- owcli/src/main.rs
+++ owcli/src/main.rs
@@ -1,16 +1,28 @@
 extern crate owlib;
 
+use std::env::args;
+
 use crate::owlib::open_window::measurement::*;
 
 fn main() {
+    let indoor_temperature = args().nth(1).expect("Indoor temperature is required");
+    let indoor_temperature: f64 = indoor_temperature.parse().expect("Indoor temperature must be numeric");
+    let indoor_humidity = args().nth(2).expect("Indoor humidity is required");
+    let indoor_humidity: f64 = indoor_humidity.parse().expect("Indoor humidity must be numeric");
+
+    let outdoor_temperature = args().nth(3).expect("Outdoor temperature is required");
+    let outdoor_temperature: f64 = outdoor_temperature.parse().expect("Outdoor temperature must be numeric");
+    let outdoor_humidity = args().nth(4).expect("Outdoor humidity is required");
+    let outdoor_humidity: f64 = outdoor_humidity.parse().expect("Outdoor humidity must be numeric");
+
     let indoor_measurement = Measurement {
-        temperature: 18.0,
-        relative_humidity: 55.0,
+        temperature: indoor_temperature,
+        relative_humidity: indoor_humidity,
     };
 
     let outdoor_measurement = Measurement {
-        temperature: 1.8,
-        relative_humidity: 90.0,
+        temperature: outdoor_temperature,
+        relative_humidity: outdoor_humidity,
     };
 
     let indoor_dew_point = indoor_measurement.calculate_dew_point();
